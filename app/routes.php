23e44b3617101e948e3456d5b972482ceacbdd0d
augustomiyagi <augusto.miyagi@gmail.com>
2015-06-14 14:25:19 -0500

    prueba de commit 1

--- app/routes.php
+++ app/routes.php
@@ -2,12 +2,13 @@
 
 Route::get('/', function()
 {
+    return "prueba de git";
     return View::make('index');
 });
 
 Route::post('test/route', function()
 {
-    return "test";
+    return "test ";
 });
 
 
