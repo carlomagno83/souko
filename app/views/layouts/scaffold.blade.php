4d1d91c6e2f30f2836fa60fa8e0f478909913964
Jorge <aztecsuministros@gmail.com>
2016-03-12 01:02:33 -0500

    elimina viewport

--- app/views/layouts/scaffold.blade.php
+++ app/views/layouts/scaffold.blade.php
@@ -3,7 +3,9 @@
 <head>
   <meta charset="utf-8">
   <title>Sistema de Almacén</title>
+  <!--
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  -->
   <meta name="description" content="">
   <meta name="author" content="">
 
